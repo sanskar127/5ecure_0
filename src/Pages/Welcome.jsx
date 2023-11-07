c6c2c14725cf079909a58849b9baf797e75f76c6
Sanskar Pandey <sanskarpandey281@gmail.com>
2023-11-07 22:41:16 +0530

    Welcome Page

---
+++ src/Pages/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './welcome.css'
+
+const Welcome = () => {
+    return (
+        <div className='welcome'>
+            <h1>Welcome!</h1>
+            <div className="navigate">
+                <span>Scroll down</span>
+                <button></button>
+            </div>
+        </div>
+    )
+}
+
+export default Welcome
