b69deae66981da5259ce186220a3d786812614c4
Sanskar Pandey <sanskarpandey281@gmail.com>
2023-11-19 15:09:44 +0530

    Dashboard Page Completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './stylesheet.css'
 import SearchIcon from '../assets/search.svg'
+import Notification from '../assets/notification.svg'
+import User from '../assets/user.svg'
 
 const Navbar = () => {
   return (
@@ -9,6 +11,15 @@ const Navbar = () => {
       <div className="search">
         <img src={SearchIcon} alt="search icon" className="searchIcon" />
         <input type="search" name="search" placeholder='Search' />
+      </div>
+      <div className="nav-actions">
+        <button className="noti">
+          <img src={Notification} alt="Notification Bell" />
+        </button>
+        <button className="user">
+          <img src={User} alt="User Icon" />
+          <h3>Username</h3>
+        </button>
       </div>
     </nav>
   )
