837ced0a496c87349ae1355e014ad9d0ca0683cd
Sanskar Pandey <sanskarpandey281@gmail.com>
2023-12-10 17:13:51 +0530

    switching logic fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './stylesheet.css'
 import SearchIcon from '../assets/search.svg'
 import Notification from '../assets/notification.svg'
 import User from '../assets/user.svg'
+import { Link } from 'react-router-dom'
+
+const UserItems = [
+  {
+    "name": "Edit Profile",
+    "action": "/editprofile"
+  },
+  {
+    "name": "Configuration",
+    "action": "/settings"
+  },
+  {
+    "name": "Logout",
+    "action": "/"
+  }
+]
+
+const Notifications = [
+  {
+    "name": "Update your Steam Password",
+    "action": "/steamactions"
+  },
+  {
+    "name": "EA Account is Vulnerable",
+    "action": "/eaactions"
+  },
+  {
+    "name": "You haven't visited me",
+    "action": "/twitchactions"
+  }
+]
+
+const NavExpand = ({ children }) => {
+  return (
+    <div className="expands">
+      {children.map(item => (
+        <Link to={item.action} key={item.id} className='link' >{item.name}</Link>
+      ))}
+    </div>
+  )
+}
 
 const Navbar = () => {
+  const [expand, setExpand] = useState(0)
+
+  useEffect(() => console.log(expand))
   return (
     <nav>
       <div className="logo">5e©ure_0?</div>
@@ -13,14 +57,16 @@ const Navbar = () => {
         <input type="search" name="search" placeholder='Search' />
       </div>
       <div className="nav-actions">
-        <button className="noti">
+        <button className="noti" onClick={() => setExpand((expand === 0 || expand === 2) ? 1 : 0)}>
           <img src={Notification} alt="Notification Bell" />
         </button>
-        <button className="user">
+        <button className="user" onClick={() => setExpand((expand === 0 || expand === 1) ? 2 : 0)}>
           <img src={User} alt="User Icon" />
           <h3>Sanskar</h3>
         </button>
+        {expand !== 0 && <NavExpand>{expand === 2 ? UserItems : Notifications}</NavExpand>}
       </div>
+
     </nav>
   )
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import './stylesheet.css'
 import Backend from './backend.json'
-import SidebarCell from './SidebarCell'
+
+const Cell = ({ name, icon, action = null }) => {
+  return (
+    <div className='cell' onClick={action}>
+      <img src={icon} alt={name} />
+      <h4>{name}</h4>
+    </div>
+  )
+}
 
 const Sidebar = () => {
   return (
     <section className='sidebar'>
       <div className="sidebar-heading">Accounts</div>
       <div className="sidebar-body">
-        {Backend.map(item => <SidebarCell key={item.id} name={item.name} icon={item.icon} />)}
+        {Backend.map(item => <Cell key={item.id} name={item.name} icon={item.icon} />)}
       </div>
     </section>
   )
